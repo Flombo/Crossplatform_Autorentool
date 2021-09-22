2b5879098ddda5f128166e5666ca2ace52d46c81
Flombo <florian-pfuetzenreuter@hotmail.de>
2021-09-22 12:52:17 +0200

    Added TooltipPopup and the tooltips for the MainPage, ContentsPage and the CreateOrEditResidentPage.

---
+++ Autorentool-RMT/Autorentool-RMT/Views/Popups/TooltipPopup.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Autorentool_RMT.Models;
+using Autorentool_RMT.ViewModels.PopupViewModels;
+using System.Collections.Generic;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms.Xaml;
+
+namespace Autorentool_RMT.Views.Popups
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TooltipPopup : Popup
+    {
+        public TooltipPopup(List<Tooltip> tooltips)
+        {
+            BindingContext = new TooltipPopupViewModel(tooltips);
+            InitializeComponent();
+        }
+
+        private void OnCloseButtonClicked(object sender, System.EventArgs e)
+        {
+            Dismiss(null);
+        }
+    }
+}
