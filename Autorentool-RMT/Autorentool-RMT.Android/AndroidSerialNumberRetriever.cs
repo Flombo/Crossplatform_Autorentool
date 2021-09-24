58ba42ad09556327f54ea801499436ebf408ce89
Flombo <florian-pfuetzenreuter@hotmail.de>
2021-09-24 15:14:48 +0200

    Created ISerialNumberRetriever implementations for Android and iOS.

---
+++ Autorentool-RMT/Autorentool-RMT.Android/AndroidSerialNumberRetriever.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using static Android.Provider.Settings;
+
+[assembly: Dependency(typeof(Autorentool_RMT.Droid.AndroidSerialNumberRetriever))]
+namespace Autorentool_RMT.Droid
+{
+    public class AndroidSerialNumberRetriever : ISerialNumberRetriever
+    {
+        #region GetDeviceSerialNumber
+        /// <summary>
+        /// Returns the AndroidID as DeviceSerialNumber.
+        /// </summary>
+        /// <returns></returns>
+        public string GetDeviceSerialNumber() => Secure.GetString(Android.App.Application.Context.ContentResolver, Secure.AndroidId);
+        #endregion
+
+    }
+}
---
+++ Autorentool-RMT/Autorentool-RMT.iOS/IOSSerialNumberRetriever.cs
@@ -0,0 +1,22 @@
+﻿using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Autorentool_RMT.iOS.IOSSerialNumberRetriever))]
+namespace Autorentool_RMT.iOS
+{
+    public class IOSSerialNumberRetriever : ISerialNumberRetriever
+    {
+
+        #region GetDeviceSerialNumber
+        /// <summary>
+        /// Returns the IdentifierForVendor as the serial-number.
+        /// </summary>
+        /// <returns></returns>
+        public string GetDeviceSerialNumber()
+        {
+            return UIDevice.CurrentDevice.IdentifierForVendor.AsString();
+        }
+        #endregion
+
+    }
+}
