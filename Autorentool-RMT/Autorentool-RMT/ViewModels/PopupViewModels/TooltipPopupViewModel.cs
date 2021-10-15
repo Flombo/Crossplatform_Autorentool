789fccc5f1954e4c3f66fbe8fd5b8edcc67c9700
Flombo <florian-pfuetzenreuter@hotmail.de>
2021-10-15 16:10:52 +0200

    Added missing TooltipPopupViewModel

---
+++ Autorentool-RMT/Autorentool-RMT/ViewModels/PopupViewModels/TooltipPopupViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Autorentool_RMT.Models;
+using System.Collections.Generic;
+
+namespace Autorentool_RMT.ViewModels.PopupViewModels
+{
+    public class TooltipPopupViewModel : ViewModel
+    {
+
+        private List<Tooltip> tooltips;
+        
+        public TooltipPopupViewModel(List<Tooltip> tooltips)
+        {
+            this.tooltips = tooltips;
+        }
+
+        #region Tooltips
+        public List<Tooltip> Tooltips
+        {
+            get => tooltips;
+        }
+        #endregion
+
+    }
+}
